7488be048fea1eb81cbd146bf67163bcc01feb91
Brigette LeVert <bnl.levert@gmail.com>
2020-01-13 00:49:11 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  
+  # setter method where the argument is set equal to the instance variable 
+  def name=(dog_name)
+    @name = dog_name
+  end
+  
+  
+  # getter method where the instance variable in the setter method is ?
+  def name
+    @name
+  end 
+  
+end 
+
